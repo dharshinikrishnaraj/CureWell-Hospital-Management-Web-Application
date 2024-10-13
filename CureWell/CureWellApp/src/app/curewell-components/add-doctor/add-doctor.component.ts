a09e84c98c11e326cc917b3442c2100fa57e54fa
dharshinikrishnaraj <dharshinikrishnraj@gmail.com>
2024-10-13 13:10:16 +0530

    add doctor ts file coded

--- CureWell/CureWellApp/src/app/curewell-components/add-doctor/add-doctor.component.ts
+++ CureWell/CureWellApp/src/app/curewell-components/add-doctor/add-doctor.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IDoctor } from '../../curewell-interfaces/doctor';
+import { CurewellService } from '../../curewell-services/curewell.service';
 
 @Component({
   selector: 'app-add-doctor',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './add-doctor.component.css'
 })
 export class AddDoctorComponent {
+
+  doctorList: IDoctor[] = [];
+  status: boolean = false;
+  showMsgDiv: boolean = false;
+  msg: string = "";
+  errAddMsg: string = "";
+
+  constructor(private _service: CurewellService){}
+
+
+  addDoctor(doctorId : number, doctorName: string){
+    this._service.addDoctor(doctorId, doctorName).subscribe(
+      responseData => {
+        this.status = responseData;
+        this.showMsgDiv = true;
+        this.msg = "Doctor successfully added";
+      },
+      responseError => {
+        this.errAddMsg = responseError;
+        this.msg = "Some error occured"
+      },
+      ()=> console.log("Add Doctor completed")
+    )
+  }
 
 }
